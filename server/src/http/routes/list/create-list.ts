08b07dadf1d90286a7534b6aa22a14f022c64d09
Matheus <ymatheus.h@gmail.com>
2024-02-17 04:28:45 -0300

    feat: :sparkles: implements route create list

---
+++ server/src/http/routes/list/create-list.ts
@@ -0,0 +1,18 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { StatusCodes } from 'http-status-codes'
+import z from 'zod'
+
+export async function createList(app: FastifyInstance) {
+  app.post('/list', async (request, reply) => {
+    const requestBodySchema = z.object({
+      name: z.string(),
+    })
+
+    const { name } = requestBodySchema.parse(request.body)
+
+    const list = await prisma.list.create({ data: { name } })
+
+    return reply.status(StatusCodes.CREATED).send({ ...list })
+  })
+}
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,11 +1,15 @@
 import Fastify from 'fastify'
+import { createList } from './routes/list/create-list'
 
 const app = Fastify()
 
+app.register(createList)
+
 app.listen({ port: 3000 }, function (err, address) {
   if (err) {
-    app.log.error(err)
+    console.error(err)
     process.exit(1)
   }
-  app.log.info(`server listening on ${address}`)
+
+  console.log(`server listening on ${address}`)
 })

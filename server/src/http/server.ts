8c2777856d28c7f48441867701e84d4402470b96
Matheus <ymatheus.h@gmail.com>
2024-02-19 21:14:27 -0300

    feat: :sparkles: Update server port to 3333

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -18,7 +18,7 @@ app.register(getListTasks)
 app.register(updateTask)
 app.register(deleteTask)
 
-app.listen({ port: 3000 }, function (err, address) {
+app.listen({ port: 3333 }, function (err, address) {
   if (err) {
     console.error(err)
     process.exit(1)
